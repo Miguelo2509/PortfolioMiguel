b344f806c6b90f9509f3b62c1c73ea3d7e9fc947
Miguelo2509 <cmiguel2509@gmail.com.com>
2022-07-04 23:45:27 -0300

    ProyectosNuevo

--- porfolio/src/app/app.module.ts
+++ porfolio/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoComponent } from './components/logo/logo.component';
-import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
@@ -21,7 +20,6 @@ import { ProyectosComponent } from './components/proyectos/proyectos.component';
     AppComponent,
     HeaderComponent,
     LogoComponent,
-    SocialComponent,
     BannerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
